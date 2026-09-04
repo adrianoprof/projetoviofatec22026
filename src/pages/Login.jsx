4bc3bf6e7ff8dac4f2daf418a292df0f36950ac2
Adriano <adrianosenai604@gmail.com>
2026-09-03 21:59:23 -0300

    fix(login): alterar mensagem inicial da tela

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -96,7 +96,7 @@ function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Vio - Testando uma alteração
+          Vio - 4DSM 2026_2
         </Typography>
         <Box
           component="form"
